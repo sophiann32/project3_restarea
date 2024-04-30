b869659dfc8e6ccb80c13112c418357ff405280c
UHB4 <153577215+UHB4@users.noreply.github.com>
2024-04-30 22:46:33 +0900

    4월30일
    노선선택박스 사이트탭으로 이동 (상위컴포넌트에서 하위컴포넌트로 props  전송)
    
    휴게소 라우트로 들어왔을때 헤더 색상 변경

--- src/kako_map/restAreaDetail.js
+++ src/kako_map/restAreaDetail.js
@@ -36,11 +36,11 @@ function Modal({ isOpen, onClose, children }) {
 }
 
 
-function RestAreaDetail() {
-    const [position, setPosition] = useState({lat: 36.5, lng: 127.5});
+
+function RestAreaDetail({ selectedRoute }) {  // props로 selectedRoute 받음
+    const [position, setPosition] = useState({ lat: 36.5, lng: 127.5 });
     const [zoomLevel, setZoomLevel] = useState(12);
     const [restAreas, setRestAreas] = useState([]);
-    const [selectedRoute, setSelectedRoute] = useState('');
     const [modalOpen, setModalOpen] = useState(false);
     const [selectedRestArea, setSelectedRestArea] = useState(null);
 
@@ -55,10 +55,11 @@ function RestAreaDetail() {
                     }
                 })
                 .catch(error => console.error('Error fetching data: ', error));
-            setZoomLevel(12);  // 노선이 변경될 때 마다 줌 레벨을 12로 설정
+            setZoomLevel(12); // 줌 레벨 설정
         }
-    }, [selectedRoute]);
+    }, [selectedRoute]); // selectedRoute 변경 시 업데이트
 
+    // 여기서부터는 이전 코드와 동일
     const mapStyle = { width: "100%", height: "900px" };
 
     const handleMarkerClick = area => {
@@ -68,58 +69,9 @@ function RestAreaDetail() {
 
     return (
         <div>
-            <select value={selectedRoute} onChange={e => setSelectedRoute(e.target.value)}>
-                <option value="">노선을 선택하세요</option>
-                <option value="동해선">동해선</option>
-                <option value="중부내륙선">중부내륙선</option>
-                <option value="호남선">호남선</option>
-                <option value="수도권제1순환선">수도권제1순환선</option>
-                <option value="울산포항선">울산포항선</option>
-                <option value="상주영덕선">상주영덕선</option>
-                <option value="서울양양선">서울양양선</option>
-                <option value="광주대구선">광주대구선</option>
-                <option value="주용로">주용로</option>
-                <option value="군도7호">군도7호</option>
-                <option value="평택제천선">평택제천선</option>
-                <option value="상주영천선">상주영천선</option>
-                <option value="지방도">지방도</option>
-                <option value="수도권제2순환선(봉담-동탄)">수도권제2순환선(봉담-동탄)</option>
-                <option value="진무로">진무로</option>
-                <option value="일반국도7호선">일반국도7호선</option>
-                <option value="익산장수선">익산장수선</option>
-                <option value="중앙선">중앙선</option>
-                <option value="밀양울산선">밀양울산선</option>
-                <option value="중앙선(대구-부산)">중앙선(대구-부산)</option>
-                <option value="인천국제공항선">인천국제공항선</option>
-                <option value="진용로">진용로</option>
-                <option value="부산외곽선">부산외곽선</option>
-                <option value="호남지선">호남지선</option>
-                <option value="부산울산선">부산울산선</option>
-                <option value="호남선(천안-논산)">호남선(천안-논산)</option>
-                <option value="국도3호선">국도3호선</option>
-                <option value="호남고속도로">호남고속도로</option>
-                <option value="대전통영선">대전통영선</option>
-                <option value="중부선">중부선</option>
-                <option value="남해선">남해선</option>
-                <option value="중부내륙">중부내륙</option>
-                <option value="중부내륙지선">중부내륙지선</option>
-                <option value="대구포항선">대구포항선</option>
-                <option value="평택시흥선">평택시흥선</option>
-                <option value="무안광주선">무안광주선</option>
-                <option value="경부선">경부선</option>
-                <option value="당진영덕선">당진영덕선</option>
-                <option value="서천공주선">서천공주선</option>
-                <option value="세종포천선(구리-포천)">세종포천선(구리-포천)</option>
-                <option value="서해안선">서해안선</option>
-                <option value="영동선">영동선</option>
-                <option value="순천완주선">순천완주선</option>
-                <option value="광주원주선">광주원주선</option>
-                <option value="서울양양선(서울-춘천)">서울양양선(서울-춘천)</option>
-                <option value="서울외곽순환선">서울외곽순환선</option>
-            </select>
             <Map center={position} level={zoomLevel} style={mapStyle}>
                 {restAreas.map((area, index) => (
-                    <MapMarker key={index} position={{lat: area.위도, lng: area.경도}}
+                    <MapMarker key={index} position={{ lat: area.위도, lng: area.경도 }}
                                onClick={() => handleMarkerClick(area)}>
                         <div>{area.휴게소명}</div>
                     </MapMarker>
--- src/routes/restArea.js
+++ src/routes/restArea.js
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './restArea.module.css';
-import RestAreaDetail from "../kako_map/restAreaDetail"; // 올바른 경로와 이름으로 임포트
+import RestAreaDetail from "../kako_map/restAreaDetail";
 
 function RestArea() {
+    const [selectedRoute, setSelectedRoute] = useState('');
+
     return (
         <>
             <div id={styles.main}>
-
                 <div className={styles.restAreaTab}>
-                    {/* RestAreaDetail 컴포넌트를 렌더링합니다 */}
+                    <select value={selectedRoute} onChange={e => setSelectedRoute(e.target.value)}>
+                        <option value="">노선을 선택하세요</option>
+                        <option value="동해선">동해선</option>
+                        <option value="중부내륙선">중부내륙선</option>
+                        <option value="호남선">호남선</option>
+                        <option value="수도권제1순환선">수도권제1순환선</option>
+                        <option value="울산포항선">울산포항선</option>
+                        <option value="상주영덕선">상주영덕선</option>
+                        <option value="서울양양선">서울양양선</option>
+                        <option value="광주대구선">광주대구선</option>
+                        <option value="주용로">주용로</option>
+                        <option value="군도7호">군도7호</option>
+                        <option value="평택제천선">평택제천선</option>
+                        <option value="상주영천선">상주영천선</option>
+                        <option value="지방도">지방도</option>
+                        <option value="수도권제2순환선(봉담-동탄)">수도권제2순환선(봉담-동탄)</option>
+                        <option value="진무로">진무로</option>
+                        <option value="일반국도7호선">일반국도7호선</option>
+                        <option value="익산장수선">익산장수선</option>
+                        <option value="중앙선">중앙선</option>
+                        <option value="밀양울산선">밀양울산선</option>
+                        <option value="중앙선(대구-부산)">중앙선(대구-부산)</option>
+                        <option value="인천국제공항선">인천국제공항선</option>
+                        <option value="진용로">진용로</option>
+                        <option value="부산외곽선">부산외곽선</option>
+                        <option value="호남지선">호남지선</option>
+                        <option value="부산울산선">부산울산선</option>
+                        <option value="호남선(천안-논산)">호남선(천안-논산)</option>
+                        <option value="국도3호선">국도3호선</option>
+                        <option value="호남고속도로">호남고속도로</option>
+                        <option value="대전통영선">대전통영선</option>
+                        <option value="중부선">중부선</option>
+                        <option value="남해선">남해선</option>
+                        <option value="중부내륙">중부내륙</option>
+                        <option value="중부내륙지선">중부내륙지선</option>
+                        <option value="대구포항선">대구포항선</option>
+                        <option value="평택시흥선">평택시흥선</option>
+                        <option value="무안광주선">무안광주선</option>
+                        <option value="경부선">경부선</option>
+                        <option value="당진영덕선">당진영덕선</option>
+                        <option value="서천공주선">서천공주선</option>
+                        <option value="세종포천선(구리-포천)">세종포천선(구리-포천)</option>
+                        <option value="서해안선">서해안선</option>
+                        <option value="영동선">영동선</option>
+                        <option value="순천완주선">순천완주선</option>
+                        <option value="광주원주선">광주원주선</option>
+                        <option value="서울양양선(서울-춘천)">서울양양선(서울-춘천)</option>
+                        <option value="서울외곽순환선">서울외곽순환선</option>
+                    </select>
                 </div>
-                <section className={styles.restAreaMap}><RestAreaDetail /></section>
+                <section className={styles.restAreaMap}>
+                    {/* RestAreaDetail에 선택된 노선명을 props로 전달 */}
+                    <RestAreaDetail selectedRoute={selectedRoute} />
+                </section>
             </div>
         </>
     );
